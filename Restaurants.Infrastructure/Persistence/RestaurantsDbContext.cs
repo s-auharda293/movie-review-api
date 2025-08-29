d7d9e4c7ecf4f6360ac8f6751e83541c222a58cc
s-auharda293 <077bct036.suharda@sagarmatha.edu.np>
2025-08-29 13:57:24 +0545

    feat: Generate migrations

--- Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
+++ Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
@@ -1,7 +1,32 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+
 
 namespace Restaurants.Infrastructure.Persistence;
 
 internal class RestaurantsDbContext: DbContext 
 {
+    internal DbSet<Restaurant> Restaurants { get; set; }
+    internal DbSet<Dish> Dishes { get; set; }
+
+    //to establish connection with database
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost\\MSSQLSERVER02;Database=RestaurantsDB;Trusted_Connection=True;TrustServerCertificate=True;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        //address is not a separate table it's properties will be stored as extra columns in 
+        //the Restaurants table
+        modelBuilder.Entity<Restaurant>().OwnsOne(r => r.Address);
+
+        //one restaurant has many dishes, WithOne means each Dish belongs to one Restaurant
+        modelBuilder.Entity<Restaurant>()
+            .HasMany(r => r.Dishes)
+            .WithOne()
+            .HasForeignKey(d => d.RestaurantId);
+    }
 }

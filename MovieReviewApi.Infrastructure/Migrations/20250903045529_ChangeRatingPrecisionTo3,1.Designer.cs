b79e2c7e0bed8eb2cc7d41cc9c19805e74154ca5
s-auharda293 <077bct036.suharda@sagarmatha.edu.np>
2025-09-03 10:48:05 +0545

    fix(rating): change rating precision from 2,1 to 3,1

--- MovieReviewApi.Infrastructure/Migrations/20250903045529_ChangeRatingPrecisionTo3,1.Designer.cs
+++ MovieReviewApi.Infrastructure/Migrations/20250903045529_ChangeRatingPrecisionTo3,1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieReviewApi.Infrastructure.Persistence;
 
@@ -11,9 +12,11 @@ using MovieReviewApi.Infrastructure.Persistence;
 namespace MovieReviewApi.Infrastructure.Migrations
 {
     [DbContext(typeof(MovieReviewDbContext))]
-    partial class MovieReviewDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250903045529_ChangeRatingPrecisionTo3,1")]
+    partial class ChangeRatingPrecisionTo31
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

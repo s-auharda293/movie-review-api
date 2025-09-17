3763f4dc093292e870b565769b7577773205c95b
s-auharda293 <077bct036.suharda@sagarmatha.edu.np>
2025-09-17 11:27:32 +0545

    feat(test): add integration test for testing creating actors and fetching all actors

---
+++ MovieReviewApi.IntegrationTests/ActorTest.cs
@@ -0,0 +1,69 @@
+﻿using MovieReviewApi.Application.DTOs;
+using System.Net.Http.Json;
+using System.Text.Json;
+using Xunit.Abstractions;
+
+namespace MovieReviewApi.IntegrationTests
+{
+    public class MovieTests : IClassFixture<MovieReviewWebApplicationFactory>
+    {
+        private readonly HttpClient _client;
+        private readonly ITestOutputHelper _output;
+
+        public MovieTests(MovieReviewWebApplicationFactory factory, ITestOutputHelper output)
+        {
+            _client = factory.CreateClient();
+            _output = output;
+            
+        }
+
+        public class Result<T>
+        {
+            public bool IsSuccess { get; set; }
+            public bool IsFailure { get; set; }
+            public T? Value { get; set; }
+            public List<string> Errors { get; set; } = new();
+        }
+
+        [Fact]
+        public async Task GetAllActors_ReturnsAllActors()
+        {
+            var response = await _client.GetAsync("/api/Actors");
+            var content = await response.Content.ReadAsStringAsync();
+
+            _output.WriteLine($"Response: {content}");
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task PostActor_ReturnsPostedActor() {
+            //arrange
+            var actorDto = new ActorDto
+            {
+                Name = "Test Doe",
+                DateOfBirth = DateTime.Parse("1990-05-15T00:00:00Z"),
+                Bio = "An actor known for action movies."
+                // MovieIds is omitted
+            };
+
+            //act
+            var response = await _client.PostAsJsonAsync("/api/Actors", actorDto);
+            //var createdActor = await response.Content.ReadFromJsonAsync<Result<ActorDto>>();
+            ////assert
+            //response.EnsureSuccessStatusCode();
+            //_output.WriteLine($"Status Code: {response.StatusCode}");
+
+            //Assert.NotNull(createdActor);
+
+            //Assert.Equal(actorDto.Name, createdActor?.Value?.Name);
+            //Assert.Equal(actorDto.DateOfBirth, createdActor?.Value?.DateOfBirth);
+            //Assert.Equal(actorDto.Bio, createdActor?.Value?.Bio);
+
+            //Assert.NotEqual(Guid.Empty, createdActor?.Value?.Id ?? Guid.Empty);
+
+            //_output.WriteLine($"Response: {JsonSerializer.Serialize(createdActor)}");
+
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
---
+++ MovieReviewApi.IntegrationTests/MovieReviewWebApplicationFactory.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using MovieReviewApi.Infrastructure.Data;
+
+namespace MovieReviewApi.IntegrationTests
+{
+    public class MovieReviewWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        //private readonly string _connectionString = @"Server=localhost\MSSQLSERVER02;Database=MovieReviewTestDb;User Id=sauharda;Password=M3w<c9]238#;TrustServerCertificate=True";
+       //customize DI and services of the app
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                // Remove existing DbContext registration
+                services.RemoveAll(typeof(DbContextOptions<MovieReviewDbContext>));
+
+                // Register test database
+                services.AddDbContext<MovieReviewDbContext>(options =>
+                {
+                    options.UseSqlServer("Server=localhost\\MSSQLSERVER02;Database=MovieReviewTestDb;User Id=sauharda;Password=M3w<c9]238#;TrustServerCertificate=True");
+                });
+
+                // Build service provider and reset test database
+                var sp = services.BuildServiceProvider();
+                using var scope = sp.CreateScope();
+                var db = scope.ServiceProvider.GetRequiredService<MovieReviewDbContext>();
+                Console.WriteLine("Resetting test database...");
+                Console.WriteLine($"Using DB: {db.Database.GetDbConnection().Database}");
+                db.Database.EnsureDeleted();
+                db.Database.Migrate();
+                Console.WriteLine("Database ready.");
+            });
+        }
+    }
+}

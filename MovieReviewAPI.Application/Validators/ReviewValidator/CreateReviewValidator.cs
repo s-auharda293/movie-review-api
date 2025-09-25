3f564ac496f521f2981e365fadaeba2d81107dae
s-auharda293 <077bct036.suharda@sagarmatha.edu.np>
2025-09-25 11:41:55 +0545

    feat(review): make comment optional but require rating

--- MovieReviewAPI.Application/Validators/ReviewValidator/CreateReviewValidator.cs
+++ MovieReviewAPI.Application/Validators/ReviewValidator/CreateReviewValidator.cs
@@ -16,9 +16,9 @@ namespace MovieReviewApi.Application.Validators.ReviewValidator
         {
 
             RuleFor(x => x.dto.Comment)
-                .NotEmpty().WithMessage("Comment is required")
                 .MinimumLength(5).WithMessage("Comment must be at least 5 characters")
-                .MaximumLength(1000).WithMessage("Comment can't exceed 1000 characters");
+                .MaximumLength(1000).WithMessage("Comment can't exceed 1000 characters")
+                .When(x => !string.IsNullOrEmpty(x.dto.Comment));
 
             RuleFor(x => x.dto.Rating)
                 .InclusiveBetween(0, 10).WithMessage("Rating must be between 0 and 10");
--- MovieReviewApi.Domain/Entities/Review.cs
+++ MovieReviewApi.Domain/Entities/Review.cs
@@ -6,7 +6,7 @@ public class Review: BaseEntity
 {
     public Guid MovieId { get; set; }     
     public Movie Movie { get; set; } = null!; 
-    public string Comment { get; set; } = null!;
+    public string? Comment { get; set; }
     public decimal Rating { get; set; }
     public string? UserId { get; set; }
     public ApplicationUser? User { get; set; }
